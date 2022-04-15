a2653f25f3a98a6d783e0e6af2079fa01b4aacf5
15931163301 <100394829+15931163301@users.noreply.github.com>
2022-04-16 01:52:00 +0800

    Update script01.js

--- script01.js
+++ script01.js
@@ -5,11 +5,13 @@
 
   function enterAnswer(){
     var answer = prompt("1+1=?");
-
+    if (answer) {
+      document.getElementById("answers").innerHTML = "Your answer is:_____" + answer;
+    }
     if (answer === "2"){
       alert("yes!!!   Go ahead and press the left button:D!!!")
     }
     else{
       alert("It seems that you can only press the button on the right : (")
-    } 
+    }
   }
