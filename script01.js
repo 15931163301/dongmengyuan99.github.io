7f6dbc3609a6915e273e1f9dbe7e22d4d5bd6268
15931163301 <100394829+15931163301@users.noreply.github.com>
2022-04-15 23:21:03 +0800

    Add files via upload

---
+++ script01.js
@@ -0,0 +1,12 @@
+
+
+  var response = prompt("what's your name?");
+  alert("Hellow "+response+" Nice to meet you. I want to share a story with you:D ");
+
+  function showTopic(){
+    var x = document.getElementById("demo");
+
+    x.style.fontSize = "50px";
+    x.style.color = "red";
+
+  }
